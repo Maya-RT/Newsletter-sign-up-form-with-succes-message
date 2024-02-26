6c5502f9e43d2f219cf67d5404fde3977cb3370e
Maya <maya.cecili@gmail.com>
2024-02-26 22:35:07 +0100

    Desktop completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const subBtn = document.querySelector(".dismiss-btn");
+const dismissBtn = document.querySelector(".dismiss-btn")
+function isValidEmail(input) {
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test
+  (input)
+}
